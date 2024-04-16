241bf62c57fc4c37ff2bb203573ab4017fa61204
shusaku009 <shusaku009@gmail.com>
2024-04-16 22:31:12 +0900

    Add: サイドバーを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Home */}
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className='Sidebar'>
+      <div className='sidebarLeft'>
+        <div className='serverIcon'>
+          <img src='./logo192.png' alt='Server Icon' />
+        </div>
+        <div className='serverIcon'>
+          <img src='./logo192.png' alt='Server Icon' />
+        </div>
+      </div>
+      <div className='sidebarRight'>
+        <div>
+          <div className='sidebarTop'>
+            <h3>Discord</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
