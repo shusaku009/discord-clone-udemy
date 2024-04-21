1e46df786a4a85f3b533f07fffd3e9285d199c28
shusaku009 <shusaku009@gmail.com>
2024-04-21 10:14:19 +0900

    Add: 必要なアイコンを追加

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 import './Sidebar.scss'
 import SidebarChannel from './SidebarChannel';
 
@@ -33,6 +36,22 @@ const Sidebar = () => {
 
           <div className='sidebarChannelList'>
             <SidebarChannel />
+          </div>
+
+          <div className='sidebarFooter'>
+            <div className='sidebarAccount'>
+              <img src='./icon.png' alt='' />
+              <div className='accountName'>
+                <h4>ShinCode</h4>
+                <span>#1234</span>
+              </div>
+            </div>
+
+            <div className='sidebarVoice'>
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
