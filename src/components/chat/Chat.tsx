066e73697de3a5a8469fa23dad59ef0d724c2651
shusaku009 <shusaku009@gmail.com>
2024-04-27 22:36:14 +0900

    Add: チャットヘッダーを作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Chat.scss'
+import ChatHeader from './ChatHeader'
 
 export default function Chat() {
   return (
     <div className='chat'>
       {/* Chat Header */}
+      <ChatHeader />
       {/* Chat Message */}
+      <div className='chatMessage'></div>
       {/* Chat Input */}
+      <div className='chatInput'></div>
     </div>
   )
 }
