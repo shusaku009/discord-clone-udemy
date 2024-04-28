d18c9bbed0072a8cbd10297654d41ff21420c7ae
shusaku009 <shusaku009@gmail.com>
2024-04-28 23:05:02 +0900

    Add: Discordアイコンを追加

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className='sidebarLeft'>
         <div className='serverIcon'>
-          <img src='./logo192.png' alt='Server Icon' />
+          <img src='./discordIcon.png' alt='Server Icon' />
         </div>
         <div className='serverIcon'>
           <img src='./logo192.png' alt='Server Icon' />
