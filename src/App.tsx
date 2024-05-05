90f1b22765b02269a6536e0e4b7edbc894ce126f
shusaku009 <shusaku009@gmail.com>
2024-05-05 23:30:12 +0900

    Add: ログインページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
+import Login from './components/login/Login';
 
 function App() {
+
+  // const user = useSelector((state) => state.user.user);
+  const user = null;
+
   return (
     <div className="App">
-      {/* Sidebar */}
-      <Sidebar />
-      {/* Home */}
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <><Login /></>
+      )}
     </div>
   );
 }
